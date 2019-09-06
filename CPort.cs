003b38b4b0af2e7fa09ea12e23c0b43a3d3a8500
Alexis Torres <54908670+AlexisTorres-85@users.noreply.github.com>
2019-09-06 15:49:32 -0500

    Receive Data Method

--- CPort.cs
+++ CPort.cs
@@ -24,13 +24,26 @@ namespace MET.COMs.App
             Port.Write(bytes, 0, bytes.Length);
             int cnt = Port.BytesToRead;
            
+            Port.DataReceived += OnReceiveData;
             Console.WriteLine("RX OL(03:38:47.146) : 85 80 02 00 FA 02 01");
             Console.WriteLine("RX OL(03:38:47.198) : 85 80 02 03 E8 01 F2");
             Console.WriteLine("RX OL(03:38:47.202) : 81 80 05 00 99 00 00 16 01 B5");
 
             Console.WriteLine("---:  129 128 5 0 153 0 0 22 1 181");
             Console.ReadLine();
-            Console.ReadLine();
+        }
+        
+        private static void OnReceiveData(object sender,SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            byte[] buffer = new byte[sp.BytesToRead];
+            Console.WriteLine("DATA RECEIVED!");
+            sp.Read(buffer, 0, buffer.Length);
+            
+            foreach (Byte bytes in buffer)
+            {
+                Console.Write(bytes.ToString() + " ");
+            }
         }
     }
 }
